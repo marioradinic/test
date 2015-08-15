05c6288d5a0220f16ef130c7f1b803b4af8a60f0
marioradinic <vgmarius@gmail.com>
2015-08-15 19:22:49 +0200

    test

---
+++ testtfs/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace testtfs.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ testtfs/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testtfs.Startup))]
+namespace testtfs
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
